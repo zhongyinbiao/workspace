b39f44d90eb61f797db3bb4fcfd3189867aecad6
z_xiansen <18501572777@163.com>
2022-04-27 15:23:43 +0800

    [ADD] add opencv config and test code
    Signed-off-by: z_xiansen <18501572777@163.com>

---
+++ cv_map/hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    while (1)
+    {
+        printf("hello.\n");
+    }
+}
---
+++ cv_map/test.cpp
@@ -0,0 +1,26 @@
+
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+    VideoCapture cap(0);
+    Mat img;
+
+    while (1)
+    {
+        cap >> img;
+        if (img.empty())
+            break;
+        namedWindow("img", WINDOW_NORMAL);
+        imshow("img", img);
+        if (27 == waitKey(20))
+            break;
+    }
+
+    return 0;
+}
+
